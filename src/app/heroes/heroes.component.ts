8c57c5122912492ba403ad70fbef7ab027ec3114
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-06-05 16:59:32 -0300

    Criado o componente Dashboard e refatoração dos componentes HeroDetail e Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,17 +13,13 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
